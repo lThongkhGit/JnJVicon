e99ad744cf077a6550da18be47fb83c10e71e8dc
LaurentTH <laurent.thongkham@streetlab-vision.com>
2024-10-22 16:06:13 +0200

    added Maze repo

--- test.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("C:/Users/lthongkh/Documents/JNJ/clinical_study_Complex/P_1019_Session_Complex/Data/Trial_29_ComplexT_True-7/Vicon/Vicon_Data_In_Vicon_Coordinate.csv", sep=';')
-print(df.columns)
